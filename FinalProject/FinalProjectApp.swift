f7aa2a1bace7b27cc1eac6de68d415d88d324fbb
Juan <chahni220700@gmail.com>
2026-06-12 09:57:06 +0800

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Punya Juan ini on 2026/6/12.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
